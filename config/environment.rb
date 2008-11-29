1ad443d9d2a1e45fcf91aae8b866d4b1b88a7b9d
Alex <blackanger.z@gmail.com>
2008-11-29 19:38:42 +0800

    modify environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -72,6 +72,4 @@ Rails::Initializer.run do |config|
   # Activate observers that should always be running
   # Please note that observers generated using script/generate observer need to have an _observer suffix
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-  
-  config.i18n.default_locale = :"zh-CN"
 end
